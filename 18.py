1b10878902eb21cfdf7c78e49991021c6c63fa82
Amrita <grace5603shama@gmail.com>
2026-08-09 19:28:26 +0530

    add eighteen program

---
+++ 18.py
@@ -0,0 +1,9 @@
+def student_details(**stu):
+
+    for key,values in stu.items():
+        print(key,":",values)
+
+
+student_details(name="jashmin",couse="MCA",age=21)
+
+#print(student_details())        
