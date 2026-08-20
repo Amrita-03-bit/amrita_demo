86a46b0ae774a6be907f18d080ddd66c434cf5f2
Amrita <grace5603shama@gmail.com>
2026-08-20 19:20:15 +0530

    Create a class Product with:
    name
    price
    
    Create a method discount() that:
    
    If price is greater than ₹1000, apply a 10% discount.
    Otherwise, apply a 5% discount.
    Return the final price.

--- 72.py
+++ 72.py
@@ -1,14 +1,26 @@
-class empolyee:
+class product:
 
-    def __init__(self,name,salary):
+    def __init__(self,name,price):
         self.name=name
-        self.salary=salary
+        self.price=price
 
-    def display(self):
-        print("name :",self.name)    
-        print("salary :",self.salary)
+    def discount(self):
 
-account_1=empolyee("rahul",40000)
-account_2=empolyee("ankit",60000)
-account_1.display()
-account_2.display()
+        if self.price>1000:
+            discount_num=self.price*10/100
+            final_price = self.price-discount_num
+            return final_price
+        
+        elif  self.price<1000:
+            discount_num = self.price*5/100
+            final_price = self.price - discount_num
+            return final_price
+        
+p1=product("manisha",50000)
+p2=product("payal",200) 
+
+print(p1.discount())
+print(p2.discount())
+          
+
+
