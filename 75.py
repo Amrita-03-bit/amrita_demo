c25e0727b338507a0a2a07fdcd3b6de9a81c0d4a
Amrita <grace5603shama@gmail.com>
2026-08-22 18:28:31 +0530

    bankaccount

--- 75.py
+++ 75.py
@@ -1,11 +1,22 @@
-employees = [
-    {"name": "Rahul", "salary": 50000},
-    {"name": "Aman", "salary": 75000},
-    {"name": "Priya", "salary": 45000},
-    {"name": "Neha", "salary": 90000}
-]
-l=[i for i in employees if i["salary"]>60000]
-for i in l:
-   i["salary"]+=i["salary"]*10/100
-   
-print(l)
+class bankaccount:
+
+    def __init__(self,name,balance):
+        self.name=name
+        self.balance=balance
+    
+    def withdraw(self,amount):
+            try:
+             if amount>self.balance:
+                print("insufficent balance")
+
+             else:
+                self.balance-=amount
+                print(self.balance)   
+            except ValueError:
+             print("abc")
+
+b1=bankaccount("shreya",40000)
+b2=bankaccount("ishika",654322)
+
+b1.withdraw(8765668)
+b2.withdraw(5434)
